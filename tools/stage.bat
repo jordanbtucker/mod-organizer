decabd0d6442517249100abeb193a45724b3826c
Tannin <Tannin@localhost>
2014-11-11 23:48:22 +0100

    - updated staging script to use qt5 and to fetch translations from transifex - removed call to function unavailable in Windows XP

--- tools/stage.bat
+++ tools/stage.bat
@@ -7,64 +7,32 @@ set /p REBUILD="Rebuild? (y/n)" %=%
 
 if /i {%REBUILD%}=={n} (goto :skipbuild)
 
-set OLDDIR=%CD%
 rmdir /s /q ..\staging_prepare
 mkdir ..\staging_prepare
-call "E:\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-set BOOSTPATH=E:\boost\boost_1_55_0
-set PYTHONPATH=E:\Python276
-set PATH=%PATH%;E:\Qt\qtcreator2.8.1\bin\;%PYTHONPATH%;%PYTHONPATH%\Lib\site-packages\PyQt4
+call "E:\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+set BOOSTPATH=E:\boost\boost_1_56_0
+set PYTHONPATH=E:\Python278
+set PATH=%PATH%;%QTDIR%\Tools\QtCreator\bin;%PYTHONPATH%;%PYTHONPATH%\Lib\site-packages\PyQt5
 set ZLIBPATH=E:\Documents\Projects\zlib-1.2.7
 set SEVENZIPPATH=E:\Documents\Projects\7zip
-set LOOTPATH=E:\Documents\Projects\loot_src
+set LOOTPATH=C:\Users\Tannin\Documents\Projects\loot_api
 
 cd ..\staging_prepare
-qmake.exe ..\source\ModOrganizer.pro -r -spec win32-msvc2010 CONFIG+=release CONFIG-=debug
+qmake.exe ..\source\ModOrganizer.pro -r -spec win32-msvc2013 CONFIG+=release CONFIG-=debug
 jom.exe
+
+cd ..\source
+..\tools\tx -q pull -a
+for /r %%f in (*.ts) do copy %%f ..\translations
+cd ..\translations
+for %%f in (*.ts) do lrelease %%f
+
 chdir /d %OLDDIR%
 
 :skipbuild
 
-
 rmdir /s /q ..\staging\ModOrganizer
 mkdir ..\staging\ModOrganizer
-
-
-set /p TRANSLATIONS="Fetch Translations? (y/n)" %=%
-
-if /i {%TRANSLATIONS%}=={n} (goto :skiptrans)
-
-set OLDDIR=%CD%
-rmdir /s /q ..\staging_trans
-mkdir ..\staging_trans
-
-call config_translations.cmd
-
-del /Q ..\staging_trans\*
-FOR %%F IN (%TRANSLATIONS%) DO (
-	wget -q -P ..\staging_trans http://translate.tannin.eu/%%F/mo/export/zip
-	e:\7-zip\7z.exe x -o..\staging_trans ..\staging_trans\mo-%%F.zip
-	chdir ..\staging_trans
-  FOR %%T IN (*.ts) DO (
-    set FILENAME=%%T
-    set LANG=%%F
-    Call :UpdateTranslation
-  )
-	ren *.ts ????????????????????_%%F.ts.x
-  copy *.ts.x ..\source\%PROJNAME%\*. >nul
-	chdir %OLDDIR%
-)
-chdir ..\staging_trans
-ren *.ts.x *.
-for %%F IN (*.ts) DO lrelease %%F >nul
-chdir %OLDDIR%
-
-:skiptrans
-
-
-set /p TRANSLATIONS="Continue?" %=%
-
-
 
 for /F "tokens=1-3* delims=." %%a in ('cscript.exe //nologo filever.vbs ..\output\ModOrganizer.exe') do @set version=%%a_%%b_%%c
 
@@ -95,21 +63,16 @@ xcopy /y /s /I static_data\* ..\staging\ModOrganizer
 xcopy /y /I ..\output\dlls\archive.dll ..\staging\ModOrganizer\dlls
 xcopy /y /I ..\output\dlls\boss.dll ..\staging\ModOrganizer\dlls
 xcopy /y /I ..\output\dlls\dlls.manifest ..\staging\ModOrganizer\dlls
+xcopy /y /s /I ..\translations\*.qm ..\staging\ModOrganizer\translations
+
+cd ..\staging\ModOrganizer
+
+windeployqt ModOrganizer.exe --no-translations --no-plugins --libdir dlls --release
+windeployqt uibase.dll --no-translations --no-plugins --libdir dlls --release
+
+chdir /d %OLDDIR%
 
 for /F "tokens=1-3* delims=." %%a in ('cscript.exe //nologo filever.vbs ..\output\ModOrganizer.exe') do echo %%a.%%b.%%c > ..\staging\version.txt
 exit /B
-
-
-:UpdateTranslation
-FOR /F "delims=" %%A in ("%FILENAME%") DO (
-  set PROJNAME=%%~nA
-)
-
-if exist ..\source\%PROJNAME% (
-  copy %FILENAME% ..\source\%PROJNAME%\%PROJNAME%_%LANG%.ts
-) else (
-  copy %FILENAME% ..\source\plugins\%PROJNAME%\%PROJNAME%_%LANG%.ts
-)
-
 
 exit /B
